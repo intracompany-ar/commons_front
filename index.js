8ae5254e30ffccf8941593c1dd5c431c8a7646dc
Alejandro Palarich <duxducisarsen@gmail.com>
2023-12-31 11:21:05 -0300

    AGreo button plus

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export * from './src/utils/validation';
 export * from './src/utils/metas';
+export {default as ButtonPlus } from './src/components/forms/ButtonPlus.vue';
---
+++ src/components/forms/ButtonPlus.vue
@@ -0,0 +1,15 @@
+<!-- SI ES MODAL: Link es el Id del modal, modal="true", onclick nada a menos que tenga que ejecutar algún script -->
+<script setup>
+const emit = defineEmits(['click']);
+const props = defineProps({
+    link: { required: false, type: String },
+    modal: { required: false, type: Boolean, default: false }
+})
+</script>
+
+<template>
+    <a v-bind:href="props.link" v-bind:data-bs-toggle="props.modal ? 'modal' : 'no_modal'" v-on:click="emit('click')"
+        accesskey="A">
+        <i class="fa fa-plus-circle fa-3x" aria-hidden="true" style="color: rgb(106,108,106);"></i>
+    </a>
+</template>
