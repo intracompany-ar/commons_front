bfabd62c03a3e834e9689efbd2156655cda3dd8b
Alejandro Palarich <duxducisarsen@gmail.com>
2024-01-02 14:24:28 -0300

    Agrego varios

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ export * from './src/utils/metas';
 export * from './src/utils/formulas';
 export * from './src/utils/dom';
 export * from './src/components/component_mounter';
+
 export { default as ButtonPlus } from './src/components/forms/buttons/ButtonPlus.vue';
 export { default as ButtonSubmit } from './src/components/forms/buttons/ButtonSubmit.vue';
+export { default as RowButtonSubmit } from './src/components/forms/buttons/RowButtonSubmit.vue';
+export { default as ButtonChat } from './src/components/forms/buttons/ButtonChat.vue';
+export { default as ButtonComment } from './src/components/forms/buttons/ButtonComment.vue';
+export { default as ButtonDirectAccess } from './src/components/forms/buttons/ButtonDirectAccess.vue';
+export { default as ButtonRefresh } from './src/components/forms/buttons/ButtonRefresh.vue';
+
+
+export { default as TextArea } from './src/components/forms/inputs/TextArea.vue';
+export { default as InputStandard } from './src/components/forms/inputs/InputStandard.vue';
 export { default as InputFechaDesde } from './src/components/forms/inputs/InputFechaDesde.vue';
 export { default as InputFechaHasta } from './src/components/forms/inputs/InputFechaHasta.vue';
+export { default as InputFecha } from './src/components/forms/inputs/InputFecha.vue';
+export { default as InputEntity } from './src/components/forms/inputs/InputEntity.vue';
+export { default as InputDescRec } from './src/components/forms/inputs/InputDescRec.vue';
+export { default as InputNumber } from './src/components/forms/inputs/InputNumber.vue';
+export { default as InputText } from './src/components/forms/inputs/InputText.vue';
 export { default as Method } from './src/components/forms/inputs/Method.vue';
 export { default as Csrf } from './src/components/forms/inputs/Csrf.vue';
+
+export { default as RadioLine } from './src/components/forms/radios/RadioLine.vue';
+
 export { default as BadgeQuantityNotifications } from './src/components/notifications/BadgeQuantityNotifications.vue';
 export { default as AdviceNoRecordsToShow } from './src/components/AdviceNoRecordsToShow.vue';
 export { default as TableStandard } from './src/components/TableStandard.vue';
 export { default as CalculadoraChequesDiferidos } from './src/components/finances/CalculadoraChequesDiferidos.vue';
+export { default as CalculadoraCuit } from './src/components/CalculadoraCuit.vue';
+export { default as ModalPpal } from './src/components/ModalPpal.vue';
 
---
+++ src/components/forms/inputs/InputEntity.vue
@@ -0,0 +1,30 @@
+<script setup>
+    const emit = defineEmits(['update:modelValue']);
+    
+    const props = defineProps({
+        id:{ required: false, type: String, default: 'entity_id' }, 
+        name:{ required: false, type: String, default: 'entity_id' },
+        small:{ required: false, type: Boolean, default: true },
+        label:{ required: false, type: Boolean, default: true },
+        modelValue:{ required: false, type: [String, Number], default: '' },
+        required:{ required: false, type: Boolean, default: false },
+        readonly:{ required: false, type: Boolean, default: false }
+    });
+</script>
+
+<template>
+    <label v-if="props.label" class="form-label" v-bind:for="props.id"><slot></slot></label>
+    <input  type="number" 
+            max="99999999999" 
+            min="9999999999"
+            class="form-control" 
+            v-bind:class="{'form-control-sm': props.small}"   
+            v-bind:name="props.name"    
+            v-bind:id="props.id" 
+            placeholder="CUIT" 
+            v-bind:value="props.modelValue"
+            v-on:input="emit('update:modelValue', $event.target.value)"
+            v-bind:required="props.required"
+            v-bind:readonly="props.readonly"
+        />
+</template>
