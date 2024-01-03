c48f19c1f83a4597b7fe26b5105bf22a149014ce
Alejandro Palarich <duxducisarsen@gmail.com>
2024-01-03 12:17:18 -0300

    Agrego opcion de pinie en component mounter

--- src/components/component_mounter.js
+++ src/components/component_mounter.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-export function mountComponent(component, id, routerParam = null) {
+export function mountComponent(component, id, routerParam = null, pinia = null) {
     const el = document.getElementById(id)
     if (!el) { return }
         const app = createApp(component, el.dataset);
+    if (pinia) {
+        const pinia = createPinia()
+        app.use(pinia);
+    }
     if (routerParam) {
         app.use(routerParam);
     }
