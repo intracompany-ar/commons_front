b650ab0d3cf2a63f55f8d99af100ede74854c4aa
Alejandro Palarich <duxducisarsen@gmail.com>
2024-01-04 18:39:29 -0300

    cursor pointer en button plus

--- src/components/forms/buttons/ButtonPlus.vue
+++ src/components/forms/buttons/ButtonPlus.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 
 <template>
     <a v-bind:href="props.link" v-bind:data-bs-toggle="props.modal ? 'modal' : 'no_modal'" v-on:click="emit('click')"
-        accesskey="A">
+        accesskey="A" style="cursor: pointer">
         <i class="fa fa-plus-circle fa-3x" aria-hidden="true" style="color: rgb(106,108,106);"></i>
     </a>
 </template>
