111730a59627e8b9b99bfb622db0526d962eef32
Alejandro Palarich <duxducisarsen@gmail.com>
2024-07-19 15:11:25 -0300

    button plus router link

--- src/components/forms/buttons/ButtonPlus.vue
+++ src/components/forms/buttons/ButtonPlus.vue
@@ -8,8 +8,9 @@ const props = defineProps({
 </script>
 
 <template>
-    <a v-bind:href="props.link" v-bind:data-bs-toggle="props.modal ? 'modal' : 'no_modal'" v-on:click="emit('click')"
+    <a  v-if="link.substring(0,4) == 'http'" v-bind:href="props.link" v-bind:data-bs-toggle="props.modal ? 'modal' : 'no_modal'" v-on:click="emit('click')"
         accesskey="A" style="cursor: pointer">
         <i class="fa fa-plus-circle fa-3x" aria-hidden="true" style="color: rgb(106,108,106);"></i>
     </a>
+    <router-link v-else :to="props.link"><i class="fa fa-plus-circle fa-3x" aria-hidden="true" style="color: rgb(106,108,106);"></i></router-link>
 </template>
