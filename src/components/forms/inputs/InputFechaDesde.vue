5273f3317bd29f6d6219b003e56656ecae49ecd0
Alejandro Palarich <duxducisarsen@gmail.com>
2024-07-26 23:17:46 -0300

    Refactorizo inputes fechas

--- src/components/forms/inputs/InputFechaDesde.vue
+++ src/components/forms/inputs/InputFechaDesde.vue
@@ -1,29 +1,22 @@
 <script setup>
-import { ref, watch } from 'vue';
+import { onMounted } from 'vue';
 import dayjs from 'dayjs'
-
-const emit = defineEmits(['update:modelValue']);
 
 const props = defineProps({
     id: { required: false, type: String, default: 'desde' },
     name: { required: false, type: String, default: 'desde' },
     big: { required: false, type: Boolean, default: false },
     dias: { required: false, type: [String, Number], default: 30 },
-    modelValue: { required: false, type: String, default: () => { return '' } }
 })
 
-const localValue = ref(props.modelValue)
-localValue.value = dayjs().subtract(props.dias, 'day').format('YYYY-MM-DD')
-
-watch(() => props.modelValue, (newVal) => { localValue.value = newVal })
-watch(localValue, (newVal) => { emit('update:modelValue', newVal) })
+const model = defineModel()
+onMounted(() => {
+    model.value = dayjs().subtract(props.dias, 'day').format('YYYY-MM-DD')
+})
 </script>
 
 <template>
-    <label class="form-label" :for="props.id">
-        <slot>Desde:</slot>
-    </label>
-    <input type="date" class="form-control" :class="{ 'form-control-sm': !props.big }" :name="props.name" :id="props.id" v-model="localValue">
-    <!-- <InputFechaDesde dias="30"></InputFechaDesde> -->
+    <label class="form-label" :for="props.id"><slot>Desde:</slot></label>
+    <input type="date" class="form-control" :class="{ 'form-control-sm': !props.big }" :name="props.name" :id="props.id" v-model="model">
 </template>
 
--- src/components/forms/inputs/InputFechaHasta.vue
+++ src/components/forms/inputs/InputFechaHasta.vue
@@ -1,27 +1,17 @@
 <script setup>
-import { ref, watch } from 'vue'
 import dayjs from 'dayjs'
 
-
-const emit = defineEmits(['update:modelValue']);
+const model = defineModel({default: dayjs().format('YYYY-MM-DD')})
 
 const props = defineProps({
     id: { required: false, type: String, default: 'hasta' },
     name: { required: false, type: String, default: 'hasta' },
-    big: { required: false, type: Boolean, default: false },
-    modelValue: { required: false, type: String, default: () => { return dayjs().format('YYYY-MM-DD') } }
+    big: { required: false, type: Boolean, default: false }
 })
 
-const localValue = ref(props.modelValue)
-
-watch(() => props.modelValue, (newVal) => { localValue.value = newVal })
-watch(localValue, (newVal) => { emit('update:modelValue', newVal) })
 </script>
 
 <template>
-    <label class="form-label" :for="id">
-        <slot>Hasta:</slot>
-    </label>
-    <input type="date" class="form-control" :class="{ 'form-control-sm': !big }" :name="name" :id="id" v-model="localValue" />
+    <label class="form-label" :for="id"><slot>Hasta:</slot></label>
+    <input type="date" class="form-control" :class="{ 'form-control-sm': !big }" :name="name" :id="id" v-model=" model" />
 </template>
-
