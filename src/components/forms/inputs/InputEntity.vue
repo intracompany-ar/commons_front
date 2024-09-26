6917bdb1e37b5f072ebf00280aea40c755e295d9
Alejandro Palarich <duxducisarsen@gmail.com>
2024-09-26 00:54:41 -0300

    Bug input entity

--- src/components/forms/inputs/InputEntity.vue
+++ src/components/forms/inputs/InputEntity.vue
@@ -1,14 +1,14 @@
 <script setup>
 const model = defineModel();
-    
-    const props = defineProps({
-        id:{ required: false, type: String, default: 'entity_id' }, 
-        name:{ required: false, type: String, default: 'entity_id' },
-        small:{ required: false, type: Boolean, default: true },
-        label:{ required: false, type: Boolean, default: true },
-        required:{ required: false, type: Boolean, default: false },
-        readonly:{ required: false, type: Boolean, default: false }
-    });
+
+const props = defineProps({
+    id:{ required: false, type: String, default: 'entity_id' }, 
+    name:{ required: false, type: String, default: 'entity_id' },
+    small:{ required: false, type: Boolean, default: true },
+    label:{ required: false, type: Boolean, default: true },
+    required:{ required: false, type: Boolean, default: false },
+    readonly:{ required: false, type: Boolean, default: false }
+});
 </script>
 
 <template>
@@ -17,12 +17,14 @@ const model = defineModel();
             max="99999999999" 
             min="9999999999"
             class="form-control" 
-            v-bind:class="{'form-control-sm': props.small}"   
-            v-bind:name="props.name"    
-            v-bind:id="props.id" 
             placeholder="CUIT" 
-            v-bind:value="model"
-            v-bind:required="props.required"
-            v-bind:readonly="props.readonly"
+            
+            :class="{'form-control-sm': props.small}"   
+            :name="props.name"    
+            :id="props.id" 
+            :required="props.required"
+            :readonly="props.readonly"
+
+            v-model="model"
         />
 </template>
