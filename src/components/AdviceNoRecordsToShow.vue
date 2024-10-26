fb85e716dc771ff91bbbd6f1689e4f2d430f5e06
Alejandro Palarich <duxducisarsen@gmail.com>
2024-10-25 23:20:34 -0300

    refactoriz aviso no records to show

--- src/components/AdviceNoRecordsToShow.vue
+++ src/components/AdviceNoRecordsToShow.vue
@@ -1,9 +1,7 @@
-<script setup>
-
-</script>
-
 <template>
-    <div class="alert alert-info text-center" role="alert">
-        No hay registros para mostrar
+    <div class="row">
+        <div class="offset-md-4 col-md-4 col-12 alert alert-info text-center p-0" role="alert">
+            No hay registros para mostrar
+        </div>
     </div>
 </template>
