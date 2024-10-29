d99bb67f121863d66ed0c868c06f9faf4b78f67d
Alejandro Palarich <duxducisarsen@gmail.com>
2024-10-29 09:51:31 -0300

    button plus refactor

--- src/components/forms/buttons/ButtonPlus.vue
+++ src/components/forms/buttons/ButtonPlus.vue
@@ -1,18 +1,28 @@
 <!-- SI ES MODAL: Link es el Id del modal, modal="true", onclick nada a menos que tenga que ejecutar algún script -->
 <script setup>
+import { computed } from 'vue';
+
+
 const emit = defineEmits(['click']);
 const props = defineProps({
     link: { required: false, type: String },
     modal: { required: false, type: Boolean, default: false }
 })
+
+const isExternalLink = computed(() => props.link?.startsWith('http'));
+const isModal = computed(() => props.modal && props.link);
 </script>
 
 <template>
-    <a v-if="link?.startsWith('http')" v-bind:href="props.link" v-bind:data-bs-toggle="props.modal ? 'modal' : 'no_modal'" v-on:click="emit('click')"
-        accesskey="A" class="plus-icon">
+    <a v-if="isExternalLink" :href="props.link" accesskey="A" class="plus-icon" target="_blank" rel="noopener noreferrer">
         <i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
     </a>
-    <router-link v-else-if="props.link" :to="props.link" class="plus-icon"><i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i></router-link>
+    <a v-else-if="isModal" :href="props.link" data-bs-toggle="modal" v-on:click="emit('click')" accesskey="A" class="plus-icon">
+        <i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
+    </a>
+    <router-link v-else-if="props.link && !props.modal" :to="props.link" class="plus-icon">
+        <i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
+    </router-link>
     <a v-else v-on:click="emit('click')" accesskey="A"  class="plus-icon">
         <i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
     </a>
