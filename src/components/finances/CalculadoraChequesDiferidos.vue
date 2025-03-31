685f6005b6c55bbf543c48e8d30e8055f1fa4154
Tu Nombre <tuemail@example.com>
2025-03-30 21:00:59 -0300

    cambio jszip a peerdenend bug4

--- src/components/finances/CalculadoraChequesDiferidos.vue
+++ src/components/finances/CalculadoraChequesDiferidos.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, computed } from "vue";
+
 import { tir, round, tnavToTnaa, tnaaToTnav, tnavToTndv, tasaRecargada_to_tnav, tasaRecargada_to_tea } from './../../utils/formulas';
 import dayjs from "dayjs";
 
