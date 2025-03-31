634259b22adfede53013d23bbd8c87f95ff6283c
Tu Nombre <tuemail@example.com>
2025-03-31 16:14:53 -0300

    Bug store_advi ts

--- src/stores/store_advices.ts
+++ src/stores/store_advices.ts
@@ -2,10 +2,11 @@ import { defineStore } from 'pinia'
 
 export const useStoreAdvices = defineStore('store_advices', () => {
 
-    function info(content: string, titulo = null) {  return content; }
-    function danger(content: string, titulo = null) { return content; }
-    function warning(content: string, titulo = null) { return content; }
-    function success(content: string, titulo = null) { return content; }
+    // Solo tiene la función de ejecutarse => dispara el  storeAdvices.$onAction(({
+    function info(content: string = '', titulo: string = '') {  return ; }
+    function danger(content: string = '', titulo: string = '') { return ; }
+    function warning(content: string = '', titulo: string = '') { return ; }
+    function success(content: string = '', titulo: string = '') { return ; }
 
     /**
      * Medio descolgado, pero lo9 puse acá para que pueda usar notificaciones, desde commons.js no se podía
