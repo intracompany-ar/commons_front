a8e322a91729e3c4b60aafef745cc91e3073f1f1
Tu Nombre <tuemail@example.com>
2025-04-02 10:58:38 -0300

    bug

--- src/utils/advicesBus.ts
+++ src/utils/advicesBus.ts
@@ -5,5 +5,6 @@ type AdviceType = 'success' | 'warning' | 'danger' | 'info'
 export const advice = ref<{ type: AdviceType, message: string | string[], title: string } | null>(null)
 
 export function showAdvice(type: AdviceType, message: string | string[], title: string = '') {
+    console.log('showAdviceEnBus', type, message )
     advice.value = { type, message, title }
 }
